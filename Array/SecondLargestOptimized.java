eb1170608c5c7891dacea23b9dca2b55745415bf
Parvez <parvezimohammed@gmail.com>
2025-03-17 08:18:36 +0530

    Second-Largest Number in an Array, Optimized Approach

---
+++ Array/SecondLargestOptimized.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class SecondLargestOptimized {
+    public static void main(String[] args) {
+        int[] arr = new int[] {1,5,5,5,5,5};
+        int n = arr.length;
+        int largest = arr[0];
+        int secondLargest = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > largest)
+                largest = arr[i];
+        }
+//        System.out.println(largest);
+        secondLargest = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > secondLargest && arr[i] != largest)
+                secondLargest = arr[i];
+        }
+        System.out.println("Second Largest: " + secondLargest);
+    }
+}
