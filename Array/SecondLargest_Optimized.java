4593254ad719b1978bdf5131424dd2f656112337
Parvez <parvezimohammed@gmail.com>
2025-03-25 23:35:59 +0530

    Second Largest of an Array - Optimized Solution

---
+++ Array/SecondLargest_Optimized.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class SecondLargest_Optimized {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,3,5,3,5,1};
+        System.out.println("Second Largest Element in a Array: " + secondLargestElement(arr));
+    }
+
+    private static int secondLargestElement(int[] arr) {
+        int n = arr.length;
+        int largest = arr[0];
+        int secondLargest = -1;
+        for (int i = 1; i < n; i++) {
+            if(arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] < largest) {
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
+    }
+}
