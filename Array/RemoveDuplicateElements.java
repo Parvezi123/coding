092ac9e990f348cef84829e523249bec75cff4bc
Parvez <parvezimohammed@gmail.com>
2025-03-28 23:59:20 +0530

    Remove Duplicate Elements from an Array - 1

--- Array/RemoveDuplicateElements.java
+++ Array/RemoveDuplicateElements.java
@@ -5,6 +5,11 @@ import java.util.Arrays;
 public class RemoveDuplicateElements {
     public static void main(String[] args) {
         int[] arr = new int[] {1,2,3,3,2,4};
+        int[] newArr = removeDuplicateElementsInArray(arr);
         System.out.println("The unique Elements Array: " + Arrays.toString(arr));
+    }
+
+    static int[] removeDuplicateElementsInArray(int[] arr) {
+        return null;
     }
 }
