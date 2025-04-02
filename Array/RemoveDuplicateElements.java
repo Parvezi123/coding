6ab7d86dddba437211819fc6a8edbb252d80df21
Parvez <parvezimohammed@gmail.com>
2025-04-02 19:56:20 +0530

    Refactored: RemoveDuplicateElements

--- Array/RemoveDuplicateElements.java
+++ Array/RemoveDuplicateElements.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class RemoveDuplicateElements {
     public static void main(String[] args) {
-        int[] arr = new int[] {1,2,2,3};
+        int[] arr = new int[] {0,0,0,1,2,2,3,3};
         int uniqueArraySize = removeDuplicateElementsInArray(arr, arr.length);
         System.out.println("Unique Elements in an Array: " + uniqueArraySize);
         System.out.println("The unique Elements Array: " + Arrays.toString(arr));
@@ -12,15 +12,18 @@ public class RemoveDuplicateElements {
 
     private static int removeDuplicateElementsInArray(int[] arr, int n) {
         int	r=0;
+        boolean isDuplicateExists = false;
         for (int i=0;i<n;i++)
         {
             for(int j=i+1;j<n;j++)
             {
-                if(arr[i]!=arr[j])
+                if(arr[i]!=arr[j] && isDuplicateExists)
                 {
                     r=r+1;
                     arr[r] = arr[j];
+                    break;
                 }
+                else isDuplicateExists = true;
             }
         }
         return r;
