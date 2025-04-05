3af5956ccc2187205cc863d63242a8145f2b2d56
Parvez <parvezimohammed@gmail.com>
2025-04-05 22:26:52 +0530

    Optimized_Code: SecondLargest_Better

--- Array/secondLargest/SecondLargest_Better.java
+++ Array/secondLargest/SecondLargest_Better.java
@@ -2,21 +2,27 @@ package Array.secondLargest;
 
 public class SecondLargest_Better {
     public static void main(String[] args) {
-        int[] arr = new int[] {1,5,5,5,5,5};
-        int n = arr.length;
-        int largest = arr[0];
-        int secondLargest = 0;
+        int[] arr = new int[]{4, 4, 5, 3, 2, 1, 5};
+        int largestElement = largestElementOfArray(arr, arr.length);
+        int secondLargestElement = secondLargestElementOfArray(arr, arr.length, largestElement);
+        System.out.println("Largest Element: " + largestElement + " Second Largest Element: " + secondLargestElement + " of an Array");
+    }
 
+    private static int secondLargestElementOfArray(int[] arr, int n, int largestElement) {
+        int secondLargest = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > secondLargest && arr[i] < largestElement)
+                secondLargest = arr[i];
+        }
+        return secondLargest;
+    }
+
+    private static int largestElementOfArray(int[] arr, int n) {
+        int largest = arr[0];
         for (int i = 1; i < n; i++) {
             if (arr[i] > largest)
                 largest = arr[i];
         }
-//        System.out.println(largest);
-        secondLargest = arr[0];
-        for (int i = 1; i < n; i++) {
-            if (arr[i] > secondLargest && arr[i] != largest)
-                secondLargest = arr[i];
-        }
-        System.out.println("Second Largest: " + secondLargest);
+        return largest;
     }
 }
