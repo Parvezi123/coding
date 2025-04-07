65d4238a3e7075bad487796244483f55c404de2a
Parvez <parvezimohammed@gmail.com>
2025-04-07 23:38:51 +0530

    Incorrect_Code: RemoveDuplicateElements

--- Array/RemoveDuplicateElements.java
+++ Array/RemoveDuplicateElements.java
@@ -7,7 +7,7 @@ public class RemoveDuplicateElements {
         int[] arr = new int[] {0,0,0,1,2,2,3,3};
         int uniqueArraySize = removeDuplicateElementsInArray(arr, arr.length);
 //        System.out.println("Unique Elements in an Array: " + uniqueArraySize);
-//        System.out.println("The unique Elements Array: " + Arrays.toString(arr));
+        System.out.println("The unique Elements Array: " + Arrays.toString(arr));
     }
 
     private static int removeDuplicateElementsInArray(int[] arr, int n) {
@@ -18,15 +18,13 @@ public class RemoveDuplicateElements {
             for(int j=i+1;j<n;j++)
             {
 
-                if(arr[i]==arr[j])
+                if(arr[i]!=arr[j] && isDuplicateExists)
                 {
-                    r=r+1;
-                    isDuplicateExists = true;
-                } else if (arr[i] != arr[j] && isDuplicateExists)
-                {
-                    arr[r] = arr[j];
-                    r=r+1;
+                    r = r + 1;
+                    arr[j] = arr[r];
+
                 }
+                else isDuplicateExists = true;
             }
         }
         return r;
