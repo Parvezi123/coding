efdd209d82d5692511de20207d94d3f0a2afe1f1
Parvez <parvezimohammed@gmail.com>
2025-05-01 02:15:42 +0530

    RemoveDuplicateElements: Partially works

--- Array/RemoveDuplicateElements.java
+++ Array/RemoveDuplicateElements.java
@@ -4,29 +4,33 @@ import java.util.Arrays;
 
 public class RemoveDuplicateElements {
     public static void main(String[] args) {
-        int[] arr = new int[] {0,0,0,1,2,2,3,3};
-        int uniqueArraySize = removeDuplicateElementsInArray(arr, arr.length);
-//        System.out.println("Unique Elements in an Array: " + uniqueArraySize);
-//        System.out.println("The unique Elements Array: " + Arrays.toString(arr));
+        int[] arr = new int[]{0, 0, 1, 2, 2};
+        System.out.println("Original Array Elements: " + Arrays.toString(arr));
+        System.out.println("Unique Array Elements Size: " + removeDuplicateElementsInArray(arr));
+        System.out.println("First Unique Array Elements: " + Arrays.toString(arr));
     }
 
-    private static int removeDuplicateElementsInArray(int[] arr, int n) {
-        int	r=0;
-        boolean isDuplicateExists = false;
-        for (int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
+    private static int removeDuplicateElementsInArray(int[] arr) {
+        int n = arr.length;
+        int uniqueElementsArraySize = 1;
+        boolean duplicateExists = false;
+        int duplicateElementIndex = 0;
+        boolean isSwapped = true;
 
-                if(arr[i]!=arr[j] && isDuplicateExists)
-                {
-                    r = r + 1;
-                    arr[j] = arr[r];
-
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    duplicateExists = true;
+                    duplicateElementIndex = isSwapped ? j : duplicateElementIndex;
+                    isSwapped = false;
+                } else if (duplicateExists) {
+                    arr[duplicateElementIndex] = arr[j];
+                    duplicateExists = false;
+                    isSwapped = true;
+                    uniqueElementsArraySize += 1;
                 }
-                else isDuplicateExists = true;
             }
         }
-        return r;
+        return uniqueElementsArraySize;
     }
 }
