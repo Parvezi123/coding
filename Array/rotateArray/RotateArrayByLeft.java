5c2283589400af2cae51290ec44a2956c57c4709
Parvez <parvezimohammed@gmail.com>
2025-05-04 19:24:18 +0530

    LeftRotateArrayByOne: BruteForce

---
+++ Array/rotateArray/RotateArrayByLeft.java
@@ -0,0 +1,22 @@
+package Array.rotateArray;
+
+import java.util.Arrays;
+
+public class RotateArrayByLeft {
+    public static void main(String[] args) {
+        int[] arr = new int[] {2,4,6,8,10};
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        System.out.println("Left Rotated Array by One: " + Arrays.toString(rotateArray(arr)));
+    }
+
+    static int[] rotateArray(int[] arr) {
+        // Write your code here.
+        int n = arr.length;
+        int temp = arr[0];
+        for(int i=0;i<n-1;i++) {
+            arr[i] = arr[i+1];
+        }
+        arr[n-1] = temp;
+        return arr;
+    }
+}
