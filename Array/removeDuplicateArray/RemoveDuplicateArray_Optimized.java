fbc0248cd3338cbabcd852e78aaca312139d87d5
Parvez <parvezimohammed@gmail.com>
2025-05-12 00:21:35 +0530

    RemoveDuplicateArray-Optimized

---
+++ Array/removeDuplicateArray/RemoveDuplicateArray_Optimized.java
@@ -0,0 +1,22 @@
+package Array.removeDuplicateArray;
+
+import java.util.Arrays;
+
+public class RemoveDuplicateArray_Optimized {
+    public static void main(String[] args) {
+        int[] arr = new int[] {2,2,3,3,3,4,5,5,6,6,7};
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        System.out.println("No. of unique Elements : " + removeDuplicateElements(arr) + " Modifies Array: " + Arrays.toString(arr));
+    }
+
+    private static int removeDuplicateElements(int[] arr) {
+        int j = 1; //At least one unique element exists as default
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != arr[j-1]) {
+                arr[j] = arr[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
