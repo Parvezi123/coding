4ddc6aac6f0566af084f568c9447093a8854904d
Parvez <parvezimohammed@gmail.com>
2025-05-15 22:10:31 +0530

    RemoveDuplicateArray-Optimized-diff-Input

--- Array/removeDuplicateArray/RemoveDuplicateArray_Optimized.java
+++ Array/removeDuplicateArray/RemoveDuplicateArray_Optimized.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class RemoveDuplicateArray_Optimized {
     public static void main(String[] args) {
-        int[] arr = new int[] {2,2,2,2,2,3,3,4,5,6,7};
+        int[] arr = new int[] {2,2,2,2,3,3,4,5,6,6,7};
         System.out.println("Original Array: " + Arrays.toString(arr));
         System.out.println("No. of unique Elements : " + removeDuplicateElements(arr) + " Modifies Array: " + Arrays.toString(arr));
     }
