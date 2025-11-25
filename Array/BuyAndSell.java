be0ca79d0baef048abfba69a198f032f8f36c684
Parvez Alam <parvez.alam-i@siemens.com>
2025-11-25 15:52:22 +0530

    Optimized the Buy and Sell Problem

--- Array/BuyAndSell.java
+++ Array/BuyAndSell.java
@@ -4,29 +4,31 @@ public class BuyAndSell {
     public static void main(String[] args) {
         int[] prices = new int[] {15000, 9000, 7500, 23000, 26500, 7387, 9600};
 
+        int maxProfit = buyAndSell(prices);
+        System.out.println("Max Profit: " + maxProfit);
+    }
+
+    static int buyAndSell (int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+
         int left = 0;
         int right = 1;
-        int buy = prices[left];
-        int sell = prices[right];
         int profit = 0;
-        int n = prices.length;
-
-        while (right < n-1) {
-
+        while (right < prices.length) {
             if (prices[left] < prices[right]) {
-
-                if (profit < (prices[right] - prices[left])) {
-                    profit = prices[right] - prices[left];
-                    buy = prices[left];
-                    sell = prices[right];
-                }
-                right+=1;
+                    int newProfit = prices[right] - prices[left];
+                    if (profit < newProfit) {
+                        profit = newProfit;
+                    }
+                    right++;
             }
             else {
-                left+=1;
-                right+=1;
+                left = right;
+                right++;
             }
         }
-        System.out.println("Buy: " + buy + " Sell: " + sell + " Profit: " + profit);
+        return profit;
     }
 }
